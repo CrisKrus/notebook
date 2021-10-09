d161f3c0b50ff6b01fb323ba5c88c8b1f6c31b7a
Cristian Suárez Vera <cristian.suarez.ulpgc@gmail.com>
2021-10-09 15:41:25 +0100

    add scripts to run the proyect

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 touch .env
 
-docker build -t cristian_suarez_dev/jupyter -f .docker/jupyter/Dockerfile .
+docker build -t cristian_suarez_dev/jupyterlab -f .docker/jupyter/Dockerfile .
 
-docker run --rm --user root --env-file .env -p 8888:8888 -v $(pwd):/home/cristian_suarez_dev/jupyterlab -e NB_USER=cristian_suarez_dev -e NB_UID=$(id -u) -e NB_GID=$(id -g) cristian_suarez_dev/jupyter
+docker run --rm --name jupyterlab --user root --env-file .env -p 8888:8888 -v $(pwd):/home/cristian_suarez_dev/jupyterlab -e NB_USER=cristian_suarez_dev -e NB_UID=$(id -u) -e NB_GID=$(id -g) cristian_suarez_dev/jupyterlab
